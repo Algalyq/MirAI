bfb0e3407568e071cfe58474d185479299190ea5
Algalyq <algalyq@mail.ru>
2023-08-05 20:16:32 +0600

    add: request render

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,19 @@ import Chat from "@/components/home/chat"
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  const backendUrl = 'http://localhost:8000'; // Replace with your actual backend API URL
+  const url = 'https://backend-project-5m5f.onrender.com'
 
+       
+  fetch(`${url}/api/`)
+  .then(response => {
+    // Handle the response here
+    console.log('GET request response:', response);
+  })
+  .catch(error => {
+    // Handle errors here
+    console.error('Error sending GET request:', error);
+  });
   return (
     session !== null ? (
       <Chat />
